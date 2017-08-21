b0294a26c0254901b41401fcf21812ee0c7b6a86
Earl Christian Bantay <bantay.earl@gmail.com>
2017-08-21 12:09:13 +0800

    Test deletion of folders direct on master file using GitHub Desktop (ebantay)

---
+++ prjDrugInventorySystem/InventorySystem/modules/dbConnection.vb
@@ -0,0 +1,19 @@
+﻿Module dbConnection
+    Public cn As New ADODB.Connection
+    Public rs As New ADODB.Recordset
+
+    Sub Connect()
+        Try
+            cn = New ADODB.Connection
+            With cn
+                If .State <> 0 Then .Close()
+                .ConnectionString = "provider=microsoft.jet.oledb.4.0;data source=../../../prjDB.mdb;Jet OLEDB:Database Password=PASSWORD;"
+                .Open()
+                ' MessageBox.Show("Connected to Database")
+            End With
+        Catch ex As Exception
+            ' MessageBox.Show("Not Connected to Database")
+        End Try
+    End Sub
+
+End Module
