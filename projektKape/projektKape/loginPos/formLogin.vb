74232b46dadeb96166173e36f084cf45c75e5a25
Earl Christian Bantay <bantay.earl@gmail.com>
2017-08-27 13:28:00 +0800

    Data not fetch on login but query and inputed user and pass are correct (ebantay)

--- projektKape/projektKape/loginPos/formLogin.vb
+++ projektKape/projektKape/loginPos/formLogin.vb
@@ -3,6 +3,10 @@
     Public global_user = vbEmpty
     Public global_pass = vbEmpty
 
+    'User inputed credentials
+    Public input_user = vbEmpty
+    Public input_pass = vbEmpty
+
     'Enables user to clear txtfields when first time using it
     Public token_user_count As Integer = 1
     Public token_pass_count As Integer = 1
@@ -10,34 +14,38 @@
 
     Private Sub formLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call connectionModule.Connect()
+
+        'Set login credentials requested by the user
+        input_user = txtUser.Text.Trim
+        input_pass = txtPass.Text.Trim
     End Sub
 
     Private Sub buttonLogin_Click(sender As Object, e As EventArgs) Handles buttonLogin.Click
-
         rs = New ADODB.Recordset
 
         With rs
             If .State <> 0 Then .Close()
-            .Open("SELECT * FROM Employees", cn, 1, 2)
+            .Open("SELECT * FROM Employees WHERE AccessUser='" + input_user + "' AND AccessPass='" + input_pass + "'", cn, 1, 2)
 
             'Catches when query finds any matching account
             If .EOF = False Then
-                global_user = .Fields("AccessUser").Value
-                global_pass = .Fields("AccessPass").Value
+                global_user = .Fields("AccessUser").Value.ToString
+                global_pass = .Fields("AccessPass").Value.ToString
 
-                If global_user.Equals(txtUser.Text.Trim) And global_pass.Equals(txtPass.Text.Trim) Then
+                MessageBox.Show(global_user + " " + global_pass)
+                If global_user.Equals(input_user) And global_pass.Equals(input_pass) Then
                     MsgBox("WELCOME ADMIN!", MsgBoxStyle.Information, "ECT Pharmacy POS")
                     Me.Hide()
-
+                    Exit Sub
                 Else
-                    MsgBox("INVALID USERNAME OR PASSWORD!", MsgBoxStyle.Critical, "ECT Pharmacy POS")
+                    MsgBox("INVALID USERNAME Or PASSWORD!", MsgBoxStyle.Critical, "ECT Pharmacy POS")
                     txtUser.Focus()
                     Exit Sub
                 End If
 
                 'Notifies the user that login fails
             Else
-                MsgBox("The username or password you've entered doesn't match with any account, please check it correctly", MsgBoxStyle.Critical, "ECT Pharmacy POS")
+                MsgBox("The username Or password you've entered doesn't match with any account, please check it correctly", MsgBoxStyle.Critical, "ECT Pharmacy POS")
                 txtUser.Focus()
                 Exit Sub
             End If
